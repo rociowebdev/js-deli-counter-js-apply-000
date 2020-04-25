41a2f8d213064a1d840393dc912c849c80fc1b4e
Rocio C <rociocordero@gmail.com>
2020-04-25 19:18:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function takeANumber(katzDeliLine, name){
+function takeANumber(katzDeliLine, name) {
     katzDeliLine.push(name);
     return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing (katzDeliLine) {
   if (katzDeliLine.length > 0) { 
-  return "Currently serving " + katzDeliLine.shift() + "."
+  return "Currently serving " + katzDeliLine.shift() + ".";
   } 
   else { 
   return "There is nobody waiting to be served!"; 
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine) {
        output += `${(i + 1)}. ${katzDeliLine[i]}, `;
       }
         output = output.slice(0, -2);
-    return `The line is currently: ${output}`;
+    return `The line is currently: ${output}.`;
   } else {
     return "The line is currently empty.";
   }
